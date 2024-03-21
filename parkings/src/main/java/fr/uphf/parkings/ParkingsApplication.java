5122f1f0457d0f0bf77d542d32d24ca1b75e74a5
PatriceAlan <peckpatricealan@gmail.com>
2024-03-21 12:44:38 +0100

    - Modification du docker-compose.yaml pour utiliser postgresql - Ajout du microservices des parkings. - Configuration efficiente des deux microservices existants avant le docker-compose.yaml

---
+++ parkings/src/main/java/fr/uphf/parkings/ParkingsApplication.java
@@ -0,0 +1,15 @@
+package fr.uphf.parkings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ParkingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingsApplication.class, args);
+	}
+
+}
---
+++ parkings/src/test/java/fr/uphf/parkings/ParkingsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.uphf.parkings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
