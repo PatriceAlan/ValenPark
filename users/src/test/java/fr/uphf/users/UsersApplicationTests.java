5f2949fbc4767240a71514ddb7c11fd9eefb178c
PatriceAlan <peckpatricealan@gmail.com>
2024-03-21 09:09:25 +0100

    Ajout du microservice des utilisateurs

---
+++ users/src/test/java/fr/uphf/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.uphf.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
