6b59aa1bd094f1db369b7d73c4458147672f4430
PatriceAlan <peckpatricealan@gmail.com>
2024-03-25 16:48:47 +0100

    modifications effectuees dans le microservice des reservations.

---
+++ reservations/src/main/java/fr/uphf/reservations/services/DTO/ParkingsFromApiDTO.java
@@ -0,0 +1,17 @@
+package fr.uphf.reservations.services.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ParkingsFromApiDTO {
+    private int idParking;
+    private String nomZoneParking;
+    private String AdresseParking;
+    private int capaciteParking;
+}
